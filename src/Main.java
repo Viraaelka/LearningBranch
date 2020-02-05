0c45bd0db8b124e4e51d04b74993f29935cff11a
Viraaelka <aelya089@mail.ru>
2020-02-05 22:16:51 +0300

    added the balanced method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Collections;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -18,16 +16,46 @@ public class Main {
         }
         return temp;
     }
-    public static int[] digitize2(long n){
+
+    public static int[] digitize2(long n) {
         String nValue = String.valueOf(n);
         int size = nValue.length();
-        int [] temp = new int[size];
-        for(int i = 0; i < size; i++){
-            temp[i] = (int) (nValue.charAt(size-i-1) - 48);
+        int[] temp = new int[size];
+        for (int i = 0; i < size; i++) {
+            temp[i] = (int) (nValue.charAt(size - i - 1) - 48);
         }
-        for(int o : temp){
+        for (int o : temp) {
             System.out.println(o);
         }
         return temp;
+    }
+
+    public static String balancedNumber(long n) {
+        String nValue = String.valueOf(n);
+        int size = nValue.length();
+        int halfSize = nValue.length() / 2;
+        int resultRight = 0, resultLeft = 0;
+        if (nValue.length() <= 2)
+            return "Balanced";
+        else {
+            if (nValue.length() % 2 == 0) {
+                for (int i = 0; i < halfSize - 1; i++) {
+                    resultRight += nValue.charAt(i) - 48;
+                }
+                System.out.println("resultRight = " + resultRight);
+            } else {
+                for (int i = 0; i < halfSize; i++) {
+                    resultRight += nValue.charAt(i) - 48;
+                }
+                System.out.println("resultRight = " + resultRight);
+            }
+            for (int i = halfSize + 1; i < size; i++) {
+                resultLeft += nValue.charAt(i) - 48;
+            }
+            System.out.println("resultLeft = " + resultLeft);
+            if (resultLeft == resultRight)
+                return "Balanced";
+            else return "Not balanced";
+        }
     }
 }
