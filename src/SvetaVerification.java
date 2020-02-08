44115321dbe3d95f7996d80315a664191e3fcafa
Viraaelka <aelya089@mail.ru>
2020-02-08 22:26:44 +0300

    added Sveta's best practices method

---
+++ src/SvetaVerification.java
@@ -0,0 +1,29 @@
+public class SvetaVerification {
+
+    public String findStep(String step, Map<String, String> rowToFind, CustomStepDef.FindStrategy strategy) {
+        Pattern groupPattern = Pattern.compile("\\{(.*?)}");
+        Matcher matcher = groupPattern.matcher(step);
+        List<String> actualSteps = new ArrayList<>();
+        while (matcher.find()) {
+            actualSteps.add(matcher.group());
+        }
+        StringBuilder expectedStep = new StringBuilder("\\{");
+        if (strategy.equals(EQUALS)) {
+            for (String key : rowToFind.keySet())
+                expectedStep = expectedStep.append("\"").append(key).append("\":\"").append(rowToFind.get(key)).append("\"(.*)");
+
+        } else if (strategy.equals(CONTAINS)) {
+            for (String key : rowToFind.keySet())
+                expectedStep = expectedStep.append("\"").append(key).append("\":\"").append(rowToFind.get(key)).append("(.*)");
+        }
+
+        for (String actualStep : actualSteps) {
+            if (actualStep.matches(expectedStep.toString()))
+                return actualStep;
+        }
+        throw new AutotestError("В транзакции не найден шаг " + expectedStep);
+    }
+
+
+
+}
